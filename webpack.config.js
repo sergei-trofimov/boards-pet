78055f48720e4d79c6ea686c505960bc77f80ee5
trofimov.s <trofimov-s@bubuntu-lx.nixsolutions.com>
2023-01-11 19:59:13 +0200

    webpack-setup: init dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+  mode: "none",
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    port: 3000,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html'
     }),
   ],
 };
