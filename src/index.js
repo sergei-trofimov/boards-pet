8bb2eb67a05e5a737782a624578fce7fb4419d06
sergei-trofimov <feast.st@gmail.com>
2023-01-12 09:26:56 +0200

    webpack-setup: Added loaders for handling styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('init')
+import './main.scss';
+
+const root = document.getElementById('root');
+
+const paragraphEl = document.createElement('p');
+paragraphEl.textContent = 'Hello World';
+paragraphEl.classList.add('red');
+
+root.appendChild(paragraphEl);
--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,19 @@ module.exports = {
       template: './src/index.html'
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  }
 };
