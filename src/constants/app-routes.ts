460a39403e8bc729c5162110bc56bbf63ad4d62e
trofimov.s <trofimov-s@bubuntu-lx.nixsolutions.com>
2023-01-26 16:17:45 +0200

    login: Add new routes

--- src/constants/app-routes.ts
+++ src/constants/app-routes.ts
@@ -2,4 +2,5 @@ import { AppRoutesInterface } from '@Models/routes/routes.interface';
 
 export const AppRoutes: AppRoutesInterface = {
   login: 'login',
+  signup: 'signup',
 };
--- src/models/routes/routes.interface.ts
+++ src/models/routes/routes.interface.ts
@@ -1,3 +1,4 @@
 export interface AppRoutesInterface {
   login: string;
+  signup: string;
 }
