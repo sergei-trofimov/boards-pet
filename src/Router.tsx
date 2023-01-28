9972e46b3b26996fc69d5698953d86b1ccf5e7dd
trofimov.s <trofimov-s@bubuntu-lx.nixsolutions.com>
2023-01-28 12:39:50 +0200

    custom: Create Auth component. Update routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { AppRoutes } from '@Constants/app-routes';
+import { AuthRoutesGuard } from '@Components/routes-guard/AuthRoutesGuard/AuthRoutesGuard';
 import { HomePage } from '@Pages/HomePage/HomePage';
 import { LoginPage } from '@Pages/Login/LoginPage';
 import { RootErrorPage } from '@Pages/RootError/RootErrorPage';
@@ -12,7 +13,13 @@ export const routerConfig: RouteObject[] = [
     element: <RootLayout />,
     errorElement: <RootErrorPage />,
     children: [
-      { index: true, element: <HomePage /> },
+      {
+        path: '/',
+        element: <AuthRoutesGuard />,
+        children: [
+          { path: '/', element: <HomePage />, children: [{ index: true, element: <p>You are authorized!</p> }] },
+        ],
+      },
       { path: AppRoutes.login, element: <LoginPage /> },
       { path: AppRoutes.signup, element: <SignUpPage /> },
     ],
