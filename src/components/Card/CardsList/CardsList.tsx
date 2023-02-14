288f1f92c35eea59d6ed312fa55705061555d15e
trofimov.s <trofimov-s@bubuntu-lx.nixsolutions.com>
2023-02-14 15:24:26 +0200

    fields: add drag-and-drop

--- src/components/Card/CardsList/CardsList.tsx
+++ src/components/Card/CardsList/CardsList.tsx
@@ -6,34 +6,52 @@ import { CardsItem } from '../CardsItem/CardsItem';
 import { CreateNewEntity } from '@Common/CreateNewEntity/CreateNewEntity';
 import { FC } from 'react';
 import { useAppSelector } from '@App-store/store';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { FieldTypeEnum } from '@Components/Field/NewField/types';
 import { Button } from '@Common/Button/Button';
+import { BaseFormFieldDisplayModel } from '@Types/form/form-data-to-display.models';
+import { DragAndDrop } from '@Common/DragAndDrop/DragAndDrop';
+
+const groupBy = 'groupBy';
 
 export const CardsList: FC = () => {
   const navigate = useNavigate();
   const params = useParams();
-  const { isLoading, cards, selectInputs, checkboxInputs } = useAppSelector((state) => {
+  const { isLoading, cards, selectInputs, checkboxInputs, board } = useAppSelector((state) => {
     const loading = state.cards.isLoading || state.boards.isLoading;
     const cards = state.cards.cards;
-    const relatedFields = (state.boards.boards.find(({ id }) => id === params.boardId)?.relatedFields || []);
+    const board = state.boards.boards.find(({ id }) => id === params.boardId);
+    const relatedFields = board?.relatedFields || [];
     const selectInputs = relatedFields.filter(({ type }) => type === FieldTypeEnum.SELECT);
     const checkboxInputs = relatedFields.filter(({ type }) => type === FieldTypeEnum.CHECKBOX);
 
-    return { isLoading: loading, cards, selectInputs, checkboxInputs }
+    return { isLoading: loading, cards, selectInputs, checkboxInputs, board };
   });
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const groupByHandler = (name: string) => {
+    if (!name) {
+      searchParams.delete(groupBy);
+      setSearchParams(searchParams);
+    } else {
+      setSearchParams({ groupBy: name });
+    }
+  };
 
   return (
     <div className="container px-10 mx-auto overflow-auto flex-grow">
       <div className="flex gap-x-3 items-baseline">
         <h4 className="font-main font-bold text-4xl text-slate-600">Group By: </h4>
-        {selectInputs.map((input) => (
-          <Button primary classNames="!w-fit px-2" key={input.id}>
-            {input.label}
-          </Button>
-        ))}
-        {checkboxInputs.map((input) => (
-          <Button primary classNames="!w-fit px-2" key={input.id}>
+        <Button primary classNames="!w-fit px-2" onClickHandler={() => groupByHandler(null)}>
+          ALL
+        </Button>
+        {selectInputs.concat(checkboxInputs).map((input: BaseFormFieldDisplayModel) => (
+          <Button
+            primary
+            classNames={`!w-fit px-2 ${searchParams.get(groupBy) === input.name && '!bg-slate-400'}`}
+            key={input.id}
+            onClickHandler={() => groupByHandler(input.name)}
+          >
             {input.label}
           </Button>
         ))}
@@ -41,15 +59,19 @@ export const CardsList: FC = () => {
       {isLoading ? (
         <Animation animationConfig={{ style: { height: '320px' } }} animationName={AnimationsName.CIRCLE_LOADER} />
       ) : cards?.length ? (
-        <div className="grid grid-cols-auto grid-rows-min-h auto-rows-m gap-5 py-3">
-          {cards.map((card: Card) => (
-            <CardsItem key={card.id} card={card} />
-          ))}
-          <CreateNewEntity
-            cardClasses="grid place-content-center cursor-pointer"
-            onClickHandler={() => navigate(`${AppRoutes.editCard.replace(':cardId', 'new')}`)}
-          />
-        </div>
+        searchParams.get(groupBy) ? (
+          <DragAndDrop board={board} cards={cards} groupBy={searchParams.get(groupBy)} />
+        ) : (
+          <div className="grid grid-cols-auto grid-rows-min-h auto-rows-m gap-5 py-3">
+            {cards.map((card: Card) => (
+              <CardsItem key={card.id} card={card} />
+            ))}
+            <CreateNewEntity
+              cardClasses="grid place-content-center cursor-pointer"
+              onClickHandler={() => navigate(`${AppRoutes.editCard.replace(':cardId', 'new')}`)}
+            />
+          </div>
+        )
       ) : (
         <CreateNewEntity
           cardClasses="w-96 h-48 flex flex-col gap-y-2 justify-center items-center cursor-pointer"
